e1cf72f2ee6b610d953a2d47d05d3f1e42331006
chengwenhao <chengwenhao@idaddy.cn>
2019-08-28 11:51:44 +0800

    fix: 修复publicPath路径

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 // vue.config.js
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/amundsen-photography/' : '/', // /amundsen-photography/是该项目下的gitpage的二级目录
 }

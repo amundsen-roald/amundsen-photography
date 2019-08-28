aae3927c9d31396556d3ce385b4a93e9322b3442
chengwenhao <chengwenhao@idaddy.cn>
2019-08-28 15:08:47 +0800

    chore: 增加eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   'extends': [
     'plugin:vue/essential',
@@ -10,7 +10,17 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/no-unused-vars': 'off',
+    'no-unused-vars': 'error',
+    'vue/attribute-hyphenation': 'warning',
+    'vue/html-quotes': 'error',
+    'vue/html-self-closing': 'off',
+    'vue/name-property-casing': 'error',
+    'vue/no-multi-spaces': 'error',
+    'no-unexpected-multiline': 'error',
+    'semi': [ 'error', 'never' ],
+    'quotes': ['error', 'single'],
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
